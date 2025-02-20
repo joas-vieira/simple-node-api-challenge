4f33198fd818c868ce19c531525e5bf8ae61853b
joas-vieira <joas.vieira.dev@gmail.com>
2025-02-20 07:49:20 -0300

    feat: init project and add crud of the tasks

---
+++ src/routes.js
@@ -0,0 +1,83 @@
+import { randomUUID } from 'node:crypto';
+import { Database } from "./database.js";
+import { buildRoutePath } from './utils/build-route-path.js';
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: 'POST',
+    url: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body;
+      
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      };
+  
+      database.insert('tasks', task);
+  
+      return res.writeHead(201).end();
+    }
+  },
+  {
+    method: 'GET',
+    url: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { search } = req.query;
+
+      const tasks = database.select('tasks', search ? { title: search, description: search } : null);
+
+      return res.end(JSON.stringify(tasks));
+    }
+  },
+  {
+    method: 'PUT',
+    url: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params;
+      const { title, description } = req.body;
+
+      const [task] = database.select('tasks', { id });
+
+      database.update('tasks', id, {
+        title: title ?? task.title,
+        description: description ?? task.description,
+        updated_at: new Date(),
+      });
+
+      return res.writeHead(204).end();
+    }
+  },
+  {
+    method: 'DELETE',
+    url: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      database.delete('tasks', id);
+
+      return res.writeHead(204).end();
+    }
+  },
+  {
+    method: 'PATCH',
+    url: buildRoutePath('/tasks/:id/complete'),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      const [task] = database.select('tasks', { id });
+
+      database.update('tasks', id, {
+        completed_at: task.completed_at ? null : new Date()
+      });
+
+      return res.writeHead(204).end();
+    }
+  }
+];
